82f75a3ea803f4707badde4d4e025eb7b66f1444
Hemerson Silveira <hemerson.dev13@gmail.com>
2025-02-04 13:49:19 -0300

    Adiciona funcionalidade para adicionar amigos à lista
    - Cria a função  que adiciona o nome do amigo à lista
    - Adiciona verificação para garantir que um nome foi inserido antes de adicionar à lista
    - Limpa o campo de entrada de texto após a adição do amigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value;
+
+    if (nome == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+    }
+    document.getElementById('amigo').value = '';
+}
+
